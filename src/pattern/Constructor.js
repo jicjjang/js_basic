df071d23e341fd2f23004a1ab27998852f8326bf
jicjjang <jicjjang12@gmail.com>
2017-01-28 22:24:43 +0900

    Add Constructor, Singleton Patterns.

---
+++ src/pattern/Constructor.js
@@ -0,0 +1,20 @@
+// This pattern include Module Pattern, Reveal Module Pattern when used to ES5.
+// (Module Paterrn == Closure.)
+
+class Constructor {
+  constructor() {
+    let variable1 = null
+    let variable2 = 1
+  }
+
+  // Es6 - Overloading is impassible
+
+  // Ess6 - Overriding is passible.
+  // Because, basically constructor is returned created null object when class haven't variables. (or functions)
+
+  temp() {
+    // ...
+  }
+}
+
+export default Constructor;
---
+++ src/pattern/Singleton.js
@@ -0,0 +1,7 @@
+// ES6 Singleton is impassible the right way. And need more than ES5 cheat.
+
+class Singleton {
+  // ....
+}
+
+export default new Singleton();
